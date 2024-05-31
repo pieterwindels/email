70636c3cef8e3ca012a3ad0e23feed7d661edb94
pieterwindels <pieter.windels@telenet.be>
2024-05-31 09:54:24 +0200

    Update email.py

--- email.py
+++ email.py
@@ -4,20 +4,18 @@ import smtplib
 
 st.header('I want full access to the app!')
 
-st.text_input("Email", key='body')
-
-#st.text_input('your email address', key='body')
-#st.button("Send email")
+#st.text_input("Email", key='body')
+email_body = st.text_input("Email")
 
 if st.button("Send new Email"):
 
         st.write('1')
 
-        if st.session_state.body == "":
+        if email_body == "":
                 st.error("Please enter your email address to request full access to the app.")
 
         else:
-                st.write(st.session_state.body)
+                st.write(email_body)
                 try:
                     # Set up the SMTP server and login to your email account
                     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -25,7 +23,7 @@ if st.button("Send new Email"):
                     server.login("devpwindels@gmail.com", "eskuahufhdrwiwzb")
                 
                     # Create the email message
-                    message = "Subject: New Email from Streamlit App\n\n" + "Email: " + st.session_state.body
+                    message = "Subject: New Email from Streamlit App\n\n" + "Email: " + email_body
                 
                     # Send the email
                     server.sendmail("devpwindels@gmail.com", "immo2smart@gmail.com", message)
